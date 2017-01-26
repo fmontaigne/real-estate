6b5975e27ca4bf3266a96a6ea694814f397e891e
Franck Montaigne <franck.montaigne@amalto.com>
2017-01-26 22:03:30 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//importing modules
+var express = require( 'express' );
+var request = require( 'request' );
+var cheerio = require( 'cheerio' );
+
+//creating a new express server
+var app = express();
+
+//setting EJS as the templating engine
+app.set( 'view engine', 'ejs' );
+
+//setting the 'assets' directory as our static assets dir (css, js, img, etc...)
+app.use( '/assets', express.static( 'assets' ) );
+
+
+//makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
+app.get( '/', function ( req, res ) {
+    res.render( 'home', {
+        message: 'The Home Page!'
+    });
+});
+
+
+//launch the server on the 3000 port
+app.listen( 3000, function () {
+    console.log( 'App listening on port 3000!' );
+});
